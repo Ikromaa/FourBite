83f45d1884f821dd2a8151c5b2713e7a98122607
Ikromaa <amorki.hataf123@gmail.com>
2026-01-14 20:11:19 +0700

    remove route admin frontend

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,10 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-    server: {
-      port: 5174,
-      strictPort: true,
-      host: true
-    }
+  plugins: [react(), tailwindcss(),]
 })
